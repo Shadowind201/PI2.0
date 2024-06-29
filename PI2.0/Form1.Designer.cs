b48e6e0fe89cf9c9b17689c07328d8c0a6cee08a
Shadowind201 <enzocarnier@gmail.com>
2024-06-28 22:21:32 -0300

    Adicionar arquivos de projeto.

---
+++ PI2.0/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace PI2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.picBanner = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBanner)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label1.Location = new System.Drawing.Point(220, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sorteie um jogo ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(199, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tenha uma sugestão aleatoria de um jogo ";
+            // 
+            // picBanner
+            // 
+            this.picBanner.Location = new System.Drawing.Point(175, 84);
+            this.picBanner.Name = "picBanner";
+            this.picBanner.Size = new System.Drawing.Size(262, 195);
+            this.picBanner.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBanner.TabIndex = 2;
+            this.picBanner.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(244, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Sortear um jogo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 354);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picBanner);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBanner)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox picBanner;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PI2.0/Form1.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Crypto;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Org.BouncyCastle.Asn1.Cmp.Challenge;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PI2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Drawing.Image[] imagens = new System.Drawing.Image[]
+{
+                System.Drawing.Image.FromFile("c:\\jogos\\1jc13gwmn0n.jpg"),
+                System.Drawing.Image.FromFile("c:\\jogos\\apex.jpg"),
+                System.Drawing.Image.FromFile("c:\\jogos\\death_stranding.jpg"),
+                System.Drawing.Image.FromFile("c:\\jogos\\fort.jpg"),
+                System.Drawing.Image.FromFile("c:\\jogos\\hollow knghit.jpg"),
+                System.Drawing.Image.FromFile("c:\\jogos\\the_isle.jpg"),
+};
+            Random geradorAleatorio = new Random();
+            int indiceAleatorio = geradorAleatorio.Next(0, imagens.Length);
+
+            picBanner.Image = imagens[indiceAleatorio];
+        }
+      private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
